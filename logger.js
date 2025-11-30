dc2d9019d8668f4485673d8be08fad2007762eb8
Jesus Beltran <a2233336114@alumnos.uat.edu.mx>
2025-11-30 14:46:25 -0600

    Delete logger.js

--- logger.js
+++
@@ -1,9 +0,0 @@
-function requestLogger(req, res, next) {
-  const timestamp = new Date().toISOString()
-  console.log(`[${timestamp}] ${req.method} ${req.path}`)
-  next()
-}
-
-module.exports = {
-  requestLogger,
-}
